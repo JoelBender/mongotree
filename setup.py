ca525b1ccb17238a2d30375884491d14f6fec543
Joel Bender <jjb5@cornell.edu>
2016-04-25 10:26:08 -0400

    remove references to a package (it's just a module) and nose, extract metadata

--- setup.py
+++ setup.py
@@ -2,24 +2,35 @@
 # -*- coding: utf-8 -*-
 
 
+import re
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
+# read the README file
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
+# read the HISTORY file
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
+
+# read the module, extract the metadata
+with open('mongotree.py') as module_file:
+    metadata = dict(
+        re.findall(
+            "__([a-z]+)__ = '([^']+)'",
+            module_file.read(),
+        ))
 
 requirements = [
     'pymongo',
 ]
 
 test_requirements = [
-    'nose',
     'bacpypes',
 ]
 
@@ -27,21 +38,15 @@ import mongotree
 
 setup(
     name='mongotree',
-    version=mongotree.__version__,
+    version=metadata['version'],
     description="Python module for tree structures in MongoDB",
     long_description=readme + '\n\n' + history,
-    author=mongotree.__author__,
-    author_email=mongotree.__email__,
+    author=metadata['author'],
+    author_email=metadata['email'],
     url='https://github.com/JoelBender/mongotree',
     py_modules=[
         'mongotree'
     ],
-#    packages=[
-#        'mongotree',
-#    ],
-#    package_dir={'mongotree':
-#                 'mongotree'},
-#    include_package_data=True,
     install_requires=requirements,
     license="MIT",
     zip_safe=False,
@@ -52,12 +57,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    test_suite='nose.collector',
+    test_suite='tests',
     tests_require=test_requirements
 )
